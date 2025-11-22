302869a2efb8f819d3f110550f9a1953c6d8ea93
MykhailoGaber <mishagaber0710@gmail.com>
2025-11-22 12:08:57 +0200

    Вирішив проблеми з модалкою

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtns: document.querySelectorAll("[data-modal-open]"), 
-    closeModalBtn: document.querySelector("[data-modal-close]"), 
-    modal: document.querySelector("[data-modal]"), 
+    openModalBtns: document.querySelectorAll("[data-modal-open1]"), 
+    closeModalBtn: document.querySelector("[data-modal-close1]"), 
+    modal: document.querySelector("[data-modal1]"), 
   };
 
   refs.closeModalBtn.addEventListener("click", toggleModal);
